37253bc36409daa1b57f9f45c4afee246a7c0da8
Calum Maclellan <mrcitytrader@gmail.com>
2025-08-07 10:28:15 +0100

    Initial Commit

---
+++ ChipDogs/ChipDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipDogsApp.swift
+//  ChipDogs
+//
+//  Created by Calum Maclellan on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChipDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
